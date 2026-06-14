5a4ea094b771ae05abd6cb24cf7db0ec5215510e
visitatvishal <visitatvishal@gmail.com>
2026-06-14 23:24:15 +0530

    Create utills_constacnt.py

---
+++ utills_constacnt.py
@@ -0,0 +1,20 @@
+REFERENCE_COLUMNS = [
+    "Reference",
+    "Assignment",
+    "Reference Key"
+]
+
+
+BANK_REFERENCE_COLUMNS = [
+    "Customer Reference",
+    "Bank Reference"
+]
+
+
+DATE_TOLERANCE_DAYS = 3
+
+
+FUZZY_THRESHOLD = 80
+
+
+MAX_COMBINATION_SIZE = 4
